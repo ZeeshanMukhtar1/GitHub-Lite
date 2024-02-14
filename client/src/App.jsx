0368f7cf8c9ae31529b6915d4853b5beb194dbab
Zeeshan Mukhtar <91063160+ZeeshanMukhtar1@users.noreply.github.com>
2024-02-14 23:55:16 +0500

    🎉 setup the entire project

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="bg-slate-700 text-3xl text-center text-blue-500 p-10 ">
+      This is Github Lite app
+    </div>
+  );
+}
+
+export default App;
