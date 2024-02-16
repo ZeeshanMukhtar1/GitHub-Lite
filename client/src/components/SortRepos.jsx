fe0f56fcea240ff4cf85e9fa92d1f5cef34b9c86
Zeeshan Mukhtar <91063160+ZeeshanMukhtar1@users.noreply.github.com>
2024-02-16 15:11:51 +0500

    🚩 optimized buttons sorting of repos

--- client/src/components/SortRepos.jsx
+++ client/src/components/SortRepos.jsx
@@ -1,33 +1,26 @@
 import React from 'react';
 
+const BUTTONS = [
+  { type: 'recent', text: 'Most Recent' },
+  { type: 'stars', text: 'Most Stars' },
+  { type: 'forks', text: 'Most Forks' },
+];
+
 const SortRepos = ({ onSort, sortType }) => {
   return (
     <div className="flex justify-center mb-2 lg:justify-end">
-      <button
-        type="button"
-        onClick={() => onSort('recent')}
-        className={`py-2.5 px-5 me-2 mb-2 text-xs sm:text-sm font-medium focus:outline-none rounded-lg bg-glass  
-        ${sortType === 'recent' ? 'border-blue-500' : ''}
-        `}
-      >
-        Most Recent
-      </button>
-      <button
-        type="button"
-        onClick={() => onSort('stars')}
-        className={`py-2.5 px-5 me-2 mb-2  text-xs sm:text-sm font-medium focus:outline-none rounded-lg bg-glass
-        ${sortType === 'stars' ? 'border-blue-500' : ''}`}
-      >
-        Most Stars
-      </button>
-      <button
-        type="button"
-        onClick={() => onSort('forks')}
-        className={`py-2.5 px-5 me-2 mb-2  text-xs sm:text-sm font-medium focus:outline-none rounded-lg bg-glass
-        ${sortType === 'forks' ? 'border-blue-500' : ''}`}
-      >
-        Most Forks
-      </button>
+      {BUTTONS.map((button) => (
+        <button
+          key={button.type}
+          type="button"
+          className={`py-2.5 px-5 me-2 mb-2 text-xs sm:text-sm font-medium focus:outline-none rounded-lg bg-glass ${
+            button.type == sortType ? 'border-blue-500' : ''
+          }`}
+          onClick={() => onSort(button.type)}
+        >
+          {button.text}
+        </button>
+      ))}
     </div>
   );
 };
