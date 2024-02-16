4f2a7b4274527541d6f7581277d53c4438e76d3f
Zeeshan Mukhtar <91063160+ZeeshanMukhtar1@users.noreply.github.com>
2024-02-16 16:52:11 +0500

    ✨ explorer page

--- client/src/components/Repos.jsx
+++ client/src/components/Repos.jsx
@@ -1,22 +1,20 @@
 import React from 'react';
 import Repo from './Repo';
 
-const Repos = ({ repos }) => {
+const Repos = ({ repos, alwaysFullScreen = false }) => {
   console.log('Received repos in Repos component:', repos);
+  const className = alwaysFullScreen ? 'w-full' : 'lg:w-2/3 w-full';
 
   return (
-    <div className={`lg:w-2/3 w-full bg-glass rounded-lg px-8 py-6`}>
+    <div className={`${className} bg-glass rounded-lg px-8 py-6`}>
       <ol className="relative border-gray-200 border-s">
-        {repos.map((repo) => {
-          return <Repo key={repo.id} repo={repo} />;
-        })}
-
+        {repos.map((repo) => (
+          <Repo key={repo.id} repo={repo} />
+        ))}
         {repos.length === 0 && (
-          <div className="flex items-center justify-center h-32">
-            <p className="text-gray-500">
-              This user has no repositories yet.🤷‍♂️{' '}
-            </p>
-          </div>
+          <p className="flex items-center justify-center h-32 ">
+            No repos found
+          </p>
         )}
       </ol>
     </div>
