98a37cb6c4e5fbb966dca67469b1a57dff0cb0c3
Zeeshan Mukhtar <91063160+ZeeshanMukhtar1@users.noreply.github.com>
2024-02-17 22:07:30 +0500

    ✨ integrated likes api in table

--- client/src/pages/LikesPage.jsx
+++ client/src/pages/LikesPage.jsx
@@ -1,6 +1,30 @@
 import { FaHeart } from 'react-icons/fa';
+import toast from 'react-hot-toast';
+import { useEffect, useState } from 'react';
+import { formatDate } from '../utils/functions';
+
+// ... (other imports)
 
 const LikesPage = () => {
+  const [likes, setLikes] = useState([]);
+
+  useEffect(() => {
+    const getLikes = async () => {
+      try {
+        const res = await fetch('/api/users/likes', { credentials: 'include' });
+        const data = await res.json();
+        if (data.error) throw new Error(data.error);
+
+        setLikes(data.likedBy);
+      } catch (error) {
+        toast.error(error.message);
+      }
+    };
+    getLikes();
+  }, []);
+
+  console.log('likes:', likes);
+
   return (
     <div className="relative px-4 overflow-x-auto rounded-lg shadow-md">
       <table className="w-full overflow-hidden text-sm text-left rtl:text-right bg-glass">
@@ -21,35 +45,35 @@ const LikesPage = () => {
           </tr>
         </thead>
         <tbody>
-          <tr className="border-b bg-glass">
-            <td className="w-4 p-4">
-              <div className="flex items-center">
-                <span>1</span>
-              </div>
-            </td>
-            <th
-              scope="row"
-              className="flex items-center px-6 py-4 whitespace-nowrap "
-            >
-              <img
-                className="w-10 h-10 rounded-full"
-                src={
-                  'https://ps.w.org/user-avatar-reloaded/assets/icon-256x256.png?rev=2540745'
-                }
-                alt="user image"
-              />
-              <div className="ps-3">
-                <div className="text-base font-semibold">Zeeshan</div>
-              </div>
-            </th>
-            <td className="px-6 py-4">02-05-24</td>
-            <td className="px-6 py-4">
-              <div className="flex items-center">
-                <FaHeart size={22} className="mx-2 text-red-500" />
-                Liked your profile
-              </div>
-            </td>
-          </tr>
+          {likes.map((user, index) => (
+            <tr key={user._id} className="border-b bg-glass">
+              <td className="w-4 p-4">
+                <div className="flex items-center">
+                  <span>{index + 1}</span>
+                </div>
+              </td>
+              <th
+                scope="row"
+                className="flex items-center px-6 py-4 whitespace-nowrap"
+              >
+                <img
+                  className="w-10 h-10 rounded-full"
+                  src={user.avatarUrl}
+                  alt="user image"
+                />
+                <div className="ps-3">
+                  <div className="text-base font-semibold">{user[0]}</div>
+                </div>
+              </th>
+              <td className="px-6 py-4">{formatDate(user.likedDate)}</td>
+              <td className="px-6 py-4">
+                <div className="flex items-center">
+                  <FaHeart size={22} className="mx-2 text-red-500" />
+                  Liked your profile
+                </div>
+              </td>
+            </tr>
+          ))}
         </tbody>
       </table>
     </div>
