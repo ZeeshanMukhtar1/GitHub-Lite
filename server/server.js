9018611bc37f5f56e3e7f28d1b5635a0fc609e30
Zeeshan Mukhtar <91063160+ZeeshanMukhtar1@users.noreply.github.com>
2024-02-23 22:56:26 +0500

    🚀 added azure deploment config

--- server/server.js
+++ server/server.js
@@ -3,6 +3,8 @@ import dotenv from 'dotenv';
 import cors from 'cors';
 import passport from 'passport';
 import session from 'express-session';
+// production imports
+import path from 'path';
 
 import './passport/github.auth.js';
 
@@ -13,6 +15,14 @@ import connectMongoDB from './db/connectMongoDB.js';
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+//productions folder structure
+const __filename = new URL(import.meta.url).pathname;
+const __dirname = path.dirname(__filename);
+
+const serverFolderPath = path.resolve(__dirname, 'server');
+const clientFolderPath = path.resolve(__dirname, 'client');
+// console.log(__dirname);
 
 app.use(
   session({ secret: 'keyboard cat', resave: false, saveUninitialized: false })
@@ -32,6 +42,15 @@ app.get('/', (req, res) => {
 app.use('/api/auth', authRouter);
 app.use('/api/users', userRouter);
 app.use('/api/explore', exploreRouter);
+
+// production scripts
+// Serve static files from the "client/dist" folder
+app.use(express.static(path.join(clientFolderPath, 'dist')));
+
+// For any other routes, serve the "index.html" file
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(clientFolderPath, 'dist', 'index.html'));
+});
 
 app.listen(PORT, () => {
   console.log('Server is running on port 5000');
